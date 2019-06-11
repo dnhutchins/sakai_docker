44da59c2c639e3d7fb54b1db80f3d92cc95aa407
David Hutchins <dnh8f@virginia.edu>
2019-06-11 14:28:54 -0400

    Restructure and add READMEs

--- build/lib/entrypoint.sh
+++ build/lib/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Append any secret properties from /run/secret.properties
+# Append any secret properties from /run/secrets/security.properties
 cat /run/secrets/security.properties >> /usr/local/tomcat/sakai/security.properties
 
 # Start tomcat
