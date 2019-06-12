b39a2778ce54fa12a6e455a61b65b6ca550c80c4
David Hutchins <dnh8f@virginia.edu>
2019-06-12 12:02:10 -0400

    Work done at and after OA conference. (#1)
    * Adding Kibana and PHPMySQLAdmin
    
    * Sprinkle on some glitter
    
    * Arrange the glitter
    
    * Tweak for demo
    
    * Initial commit of development tools stack
    
    * Remove junk
    
    * More work on dev exmaple
    
    * Add various exmaples
    
    * Not needed
    
    * Create properties folder in tomcat_entry
    
    * Exclude data directory from git
    
    * Removed submodule
    
    * Removed submodule
    
    * Setup builder interface at runtime
    
    * Add initial README
    
    * Images for README
    
    * Update README.md
    
    * Update README.md
    
    * Restructure and add READMEs
    
    * Cleanup some things
    
    * Update README.md

--- dev/lib/tomcat_entry_nogl.sh
+++ dev/lib/tomcat_entry_nogl.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sed -i '/^common.loader\=/ s/$/,"\${catalina.base}\/sakai-lib\/*.jar"/' /usr/local/tomcat/conf/catalina.properties
+
+mkdir -p /usr/local/sakai/properties
+
 # Find the correct interface for elasticsearch using the subnet defined in the compose file
 ES_INT=$(ip addr | grep 10.99.99 | cut -d " " -f 11)
 
@@ -9,10 +13,5 @@ echo "Using interface $ES_INT for elasticsearch"
 # Create the updated Sakai configuration
 sed s/#interface#/\_$ES_INT:ipv4\_/g /usr/local/sakai/es.properties > /usr/local/sakai/properties/sakai.properties
 
-rm -fr /usr/local/tomcat/webapps/ROOT
-mkdir /usr/local/tomcat/webapps/ROOT
-cd /usr/local/tomcat/webapps/ROOT
-tar zxvf /demopage.tgz
-
-# Execute the real entrypoint script
-/entrypoint.sh
+# Start tomcat
+catalina.sh run
